7b846810cb7aa4ea3be226a11085c21554f5dc93
Yura <yterletskyi@gmail.com>
2016-11-12 21:10:03 +0200

    Add SharedPreferencesManager class

---
+++ app/src/main/java/yterletskyi/alias/SharedPreferencesManager.kt
@@ -0,0 +1,42 @@
+package yterletskyi.alias
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+
+/**
+ * Created by yterletskyi on 12.11.16.
+ */
+class SharedPreferencesManager(context: Context?) {
+
+    val GAME_TIME_LENGTH_KEY: String = "game_time_length_key"
+    val GAME_FINISH_SCORE_KEY: String = "game_finish_score_key"
+
+    var mSharedPreferences: SharedPreferences? = null;
+
+    init {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+    }
+
+    fun saveGameLengthTime(seconds: Long) {
+        val editor = mSharedPreferences!!.edit()
+        editor.putLong(GAME_TIME_LENGTH_KEY, seconds)
+        editor.apply()
+    }
+
+    fun getGameLengthTime(): Long {
+        return mSharedPreferences!!.getLong(GAME_TIME_LENGTH_KEY, 0L);
+    }
+
+    fun saveGameFinishScore(seconds: Int) {
+        val editor = mSharedPreferences!!.edit()
+        editor.putInt(GAME_FINISH_SCORE_KEY, seconds)
+        editor.apply()
+    }
+
+    fun getGameFinishScore(): Int {
+        return mSharedPreferences!!.getInt(GAME_FINISH_SCORE_KEY, 0);
+    }
+
+
+}
